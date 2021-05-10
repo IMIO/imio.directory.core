851bd52471f49cd29a0d8719a6ca5ed312418a6e
Christophe Boulanger <christophe.boulanger@imio.be>
2021-05-10 13:28:29 +0200

    Add taxonomies

--- src/imio/directory/core/setuphandlers.py
+++ src/imio/directory/core/setuphandlers.py
@@ -1,21 +1,22 @@
 # -*- coding: utf-8 -*-
+from imio.directory.core.taxonomies.utils import add_contact_taxonomy
 from Products.CMFPlone.interfaces import INonInstallable
 from zope.interface import implementer
 
 
 @implementer(INonInstallable)
 class HiddenProfiles(object):
-
     def getNonInstallableProfiles(self):
         """Hide uninstall profile from site-creation and quickinstaller."""
         return [
-            'imio.directory.core:uninstall',
+            "imio.directory.core:uninstall",
         ]
 
 
 def post_install(context):
     """Post install script"""
-    # Do something at the end of the installation of this package.
+    add_contact_taxonomy("category")
+    add_contact_taxonomy("topic")
 
 
 def uninstall(context):
