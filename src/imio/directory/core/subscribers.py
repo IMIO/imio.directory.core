b85cbed002e394b84592058ebaae9aeed1471da6
Laurent Lasudry <lasudry@gmail.com>
2021-09-07 15:33:08 +0200

    Add new breadcrumb index / metadata
    This will help finding a contact with its whole breadcrumb path

--- src/imio/directory/core/subscribers.py
+++ src/imio/directory/core/subscribers.py
@@ -1,6 +1,8 @@
 # -*- coding: utf-8 -*-
 
 from imio.smartweb.common.faceted.utils import configure_faceted
+from plone import api
+from plone.app.dexterity.behaviors.metadata import IBasic
 import os
 
 
@@ -9,3 +11,18 @@ def added_entity(obj, event):
         os.path.dirname(__file__)
     )
     configure_faceted(obj, faceted_config_path)
+
+
+def modified_content(obj, event):
+    if not hasattr(event, "descriptions") or not event.descriptions:
+        return
+
+    for d in event.descriptions:
+        if d.interface is not IBasic:
+            continue
+        if "IBasic.title" in d.attributes:
+            brains = api.content.find(context=obj)
+            for brain in brains:
+                content = brain.getObject()
+                content.reindexObject(idxs=["breadcrumb"])
+            return
