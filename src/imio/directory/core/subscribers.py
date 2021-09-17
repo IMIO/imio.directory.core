d9f6ec750799318f24724e9774cf4f999b681210
Laurent Lasudry <lasudry@gmail.com>
2021-09-17 11:24:37 +0200

    Remove breadcrumb related code
    This code has moved to imio.smartweb.common as we need it for different products

--- src/imio/directory/core/subscribers.py
+++ src/imio/directory/core/subscribers.py
@@ -2,24 +2,8 @@
 
 from imio.directory.core.utils import get_entity_uid_for_contact
 from imio.smartweb.common.faceted.utils import configure_faceted
-from plone import api
-from plone.app.dexterity.behaviors.metadata import IBasic
+
 import os
-
-
-def reindex_breadcrumb(obj, event):
-    if not hasattr(event, "descriptions") or not event.descriptions:
-        return
-
-    for d in event.descriptions:
-        if d.interface is not IBasic:
-            continue
-        if "IBasic.title" in d.attributes:
-            brains = api.content.find(context=obj)
-            for brain in brains:
-                content = brain.getObject()
-                content.reindexObject(idxs=["breadcrumb"])
-            return
 
 
 def set_default_entity_uid(contact):
@@ -37,14 +21,9 @@ def added_entity(obj, event):
     configure_faceted(obj, faceted_config_path)
 
 
-def modified_entity(obj, event):
-    reindex_breadcrumb(obj, event)
-
-
 def added_contact(obj, event):
     set_default_entity_uid(obj)
 
 
 def modified_contact(obj, event):
     set_default_entity_uid(obj)
-    reindex_breadcrumb(obj, event)
