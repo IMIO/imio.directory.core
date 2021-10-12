fb2a56b20750225464bb12bfbfe218cc7729c0f0
Laurent Lasudry <lasudry@gmail.com>
2021-10-12 16:03:35 +0200

    Extend vocabularies for `@search-filters` endpoint

--- src/imio/directory/core/__init__.py
+++ src/imio/directory/core/__init__.py
@@ -1,2 +1,11 @@
 # -*- coding: utf-8 -*-
-"""Init and utils."""
+
+from imio.smartweb.common.config import VOCABULARIES_MAPPING
+
+VOCABULARIES_MAPPING.update(
+    {
+        "contact_type": "imio.directory.vocabulary.ContactTypes",
+        "facilities": "imio.directory.vocabulary.Facilities",
+        "taxonomy_contact_category": "collective.taxonomy.contact_category",
+    }
+)
