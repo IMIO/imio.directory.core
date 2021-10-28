6fd4f758ded06350584083ab5ca919f97d11af20
Laurent Lasudry <lasudry@gmail.com>
2021-10-28 16:43:00 +0200

    Remove useless logo field

--- src/imio/directory/core/contents/entity/content.py
+++ src/imio/directory/core/contents/entity/content.py
@@ -5,7 +5,6 @@ from plone import schema
 from plone.app.z3cform.widget import SelectFieldWidget
 from plone.autoform import directives
 from plone.dexterity.content import Container
-from plone.namedfile.field import NamedBlobImage
 from plone.supermodel import model
 from zope.interface import implementer
 
@@ -19,8 +18,6 @@ class IEntity(model.Schema):
         description=_(u"Choose zip codes for this entity"),
         value_type=schema.Choice(vocabulary="imio.smartweb.vocabulary.Cities"),
     )
-
-    logo = NamedBlobImage(title=_(u"Logo"), description=_(u""), required=False)
 
 
 @implementer(IEntity)
